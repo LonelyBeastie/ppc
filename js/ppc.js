5c968e18ccf2849d7e9f0911d8a5a006e0a15f71
Jake <jacobsiddall@gmail.com>
2016-09-19 16:35:06 -0500

    ppc site...yeah, I lost all of git

---
+++ js/ppc.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  var fade_time = 1000;
+  loop_slide_fading(fade_time);
+});
+
+function loop_slide_fading(fade_time){
+  slide_fading(fade_time).then(function() {
+    loop_slide_fading(fade_time);
+  });
+}
+function slide_fading(fade_time){
+  return new Promise(function (fulfill, reject){
+    $('.blur_1').fadeIn(fade_time, function() {
+      $('.blur_2').fadeIn( fade_time, function() {
+        $('.blur_3').fadeIn( fade_time, function() {
+          $('.blur_4').fadeIn( fade_time, function() {
+            $('.blur_0').fadeIn( fade_time, function() {
+              $('.blur_2, .blur_3, .blur_4, .blur_0').fadeOut( fade_time, function() {
+                fulfill(true); //if the action succeeded
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+}
