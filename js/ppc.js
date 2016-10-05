85665305ccf6e1f6dcef83be57f40d2aa5042b3b
Jake <jacobsiddall@gmail.com>
2016-10-05 12:59:12 -0500

    makes words not fade

--- js/ppc.js
+++ js/ppc.js
@@ -11,10 +11,15 @@ function loop_slide_fading(fade_time){
 }
 function slide_fading(fade_time){
   return new Promise(function (fulfill, reject){
+    $('.fade_pic_words').removeClass("not_shown")
     $('.blur_1').fadeIn(fade_time, function() {
+      $('.fade_pic_words').text("Social");
       $('.blur_2').fadeIn( fade_time, function() {
+        $('.fade_pic_words').text("Friends");
         $('.blur_3').fadeIn( fade_time, function() {
+          $('.fade_pic_words').text("Community");
           $('.blur_4').fadeIn( fade_time, function() {
+            $('.fade_pic_words').addClass("long").text("We all need neighborhoods");
             $('.blur_0').fadeIn( fade_time, function() {
               // $('.blur_2, .blur_3, .blur_4, .blur_0').fadeOut( fade_time, function() {
                 fulfill(true); //if the action succeeded
