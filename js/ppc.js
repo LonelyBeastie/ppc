dbf849c47ebb2a8eba741bdf4d7727fc275c5cc8
Jake <jacobsiddall@gmail.com>
2017-03-17 09:41:07 -0700

    makes use of bluebird.js for promises

--- js/ppc.js
+++ js/ppc.js
@@ -7,13 +7,13 @@ $(document).ready(function() {
 });
 
 function loop_slide_fading(fade_time){
-  slide_fading(fade_time).then(function() {
-    // loop_slide_fading(fade_time);
-    window.location.href = "main.html";
-  });
+  slide_fading(fade_time)//.then(function() {
+
 }
 function slide_fading(fade_time){
-  return new Promise(function (fulfill, reject){
+  var promise = new Promise(function(resolve) {
+    var fade_time = 2000;
+  // return new Promise(function (fulfill, reject){
     $('.fade_pic_words').removeClass("not_shown")
     $('.blur_1').fadeIn(fade_time, function() {
       $('.fade_pic_words').text("Social");
@@ -25,7 +25,8 @@ function slide_fading(fade_time){
             $('.fade_pic_words').addClass("long").text("We all need neighborhoods");
             $('.blur_0').fadeIn( fade_time, function() {
               // $('.blur_2, .blur_3, .blur_4, .blur_0').fadeOut( fade_time, function() {
-                fulfill(true); //if the action succeeded
+                // fulfill(true); //if the action succeeded
+                resolve(true); //if the action succeeded
               // });
             });
           });
@@ -33,4 +34,22 @@ function slide_fading(fade_time){
       });
     });
   });
+  promise.then(function(result) {
+    // loop_slide_fading(fade_time);
+    window.location.href = "main.html";
+  });
 }
+//
+// 'use strict';
+//
+//
+//     setTimeout(function() {
+//         resolve("result");
+//     }, 1000);
+// });
+//
+// promise.then(function(result) {
+//     alert("Fulfilled: " + result);
+// }, function(error) {
+//     alert("Rejected: " + error);
+// });
